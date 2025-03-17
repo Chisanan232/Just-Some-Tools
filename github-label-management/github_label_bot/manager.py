20d5edcb292996cd0184690c524e555fcc598890
Chisanan232 <chi10211201@cycu.org.tw>
2025-03-17 21:56:45 +0800

    ♻️ Wrap all the functions into an object to manage.

--- github-label-management/github_label_bot/manager.py
+++ github-label-management/github_label_bot/manager.py
@@ -11,114 +11,115 @@ from .model import GitHubLabelManagementConfig, Label as GitHubLabelBotLabel
 from ._utils.file.operation import YAML
 
 
-def load_label_config(config_path: str) -> GitHubLabelManagementConfig:
-    """Load label configuration from YAML file."""
-    with open(config_path, 'r') as file:
-        return GitHubLabelManagementConfig.serialize(yaml.safe_load(file))
+class GitHubLabelBot:
 
+    def load_label_config(self, config_path: str) -> GitHubLabelManagementConfig:
+        """Load label configuration from YAML file."""
+        with open(config_path, 'r') as file:
+            return GitHubLabelManagementConfig.serialize(yaml.safe_load(file))
 
-def sync_labels(repo: Repository, label_config: GitHubLabelManagementConfig) -> None:
-    """Synchronize repository labels with configuration."""
-    # Get existing labels
-    existing_labels: Dict[str, GitHubLabel] = {label.name: label for label in repo.get_labels()}
-    
-    # Update or create labels
-    for name, props in label_config.labels.items():
-        if name in existing_labels:
-            label = existing_labels[name]
-            if (label.color != props.color or
-                label.description != props.description):
-                label.edit(
+    def sync_labels(self, repo: Repository, label_config: GitHubLabelManagementConfig) -> None:
+        """Synchronize repository labels with configuration."""
+        # Get existing labels
+        existing_labels: Dict[str, GitHubLabel] = {label.name: label for label in repo.get_labels()}
+
+        # Update or create labels
+        for name, props in label_config.labels.items():
+            if name in existing_labels:
+                label = existing_labels[name]
+                if (label.color != props.color or
+                    label.description != props.description):
+                    label.edit(
+                        name=name,
+                        color=props.color,
+                        description=props.description
+                    )
+                    print(f"Updated label: {name}")
+            else:
+                repo.create_label(
                     name=name,
                     color=props.color,
                     description=props.description
                 )
-                print(f"Updated label: {name}")
-        else:
-            repo.create_label(
-                name=name,
-                color=props.color,
-                description=props.description
+                print(f"Created label: {name}")
+
+        # Delete labels not in config if specified
+        if label_config.delete_unused:
+            for name, label in existing_labels.items():
+                if name not in label_config.labels:
+                    label.delete()
+                    print(f"Deleted label: {name}")
+
+
+    def syncup_as_config(self) -> None:
+        # Load GitHub token from environment variable
+        print(f"[DEBUG] Get GitHub token.")
+        token = self._get_github_token()
+
+        # Initialize GitHub client
+        print(f"[DEBUG] Initial GitHub repository instance.")
+        g = Github(token)
+
+        # Load configuration
+        print(f"[DEBUG] Load the configuration.")
+        config = self.load_label_config('./test/_data/github-labels.yaml')
+
+        # Process each repository
+        print(f"[DEBUG] Start to sync up the GitHub label setting ...")
+        for repo_name in config.repositories:
+            print(f"[DEBUG] Sync up GtHub project {repo_name}")
+            try:
+                repo = g.get_repo(repo_name)
+                print(f"\nProcessing repository: {repo_name}")
+                self.sync_labels(repo, config)
+            except github.GithubException as e:
+                print(f"Error processing {repo_name}: {e}")
+
+
+    def _get_github_token(self):
+        token = os.getenv('GITHUB_TOKEN')
+        if not token:
+            raise ValueError("GITHUB_TOKEN environment variable not set")
+        return token
+
+
+    def download_labels(self, repo: github.Repository) -> None:
+        existing_labels: Dict[str, GitHubLabel] = {label.name: label for label in repo.get_labels()}
+        labels_config: Dict[str, GitHubLabelBotLabel] = {}
+        for label_name, label_info in existing_labels.items():
+            print(f"[DEBUG] Sync label {label_name}!")
+            labels_config[label_name] = GitHubLabelBotLabel(
+                color=label_info.color,
+                description=label_info.description,
             )
-            print(f"Created label: {name}")
-    
-    # Delete labels not in config if specified
-    if label_config.delete_unused:
-        for name, label in existing_labels.items():
-            if name not in label_config.labels:
-                label.delete()
-                print(f"Deleted label: {name}")
-
-
-def syncup_as_config() -> None:
-    # Load GitHub token from environment variable
-    print(f"[DEBUG] Get GitHub token.")
-    token = _get_github_token()
-
-    # Initialize GitHub client
-    print(f"[DEBUG] Initial GitHub repository instance.")
-    g = Github(token)
-
-    # Load configuration
-    print(f"[DEBUG] Load the configuration.")
-    config = load_label_config('./test/_data/github-labels.yaml')
-
-    # Process each repository
-    print(f"[DEBUG] Start to sync up the GitHub label setting ...")
-    for repo_name in config.repositories:
-        print(f"[DEBUG] Sync up GtHub project {repo_name}")
-        try:
-            repo = g.get_repo(repo_name)
-            print(f"\nProcessing repository: {repo_name}")
-            sync_labels(repo, config)
-        except github.GithubException as e:
-            print(f"Error processing {repo_name}: {e}")
-
-
-def _get_github_token():
-    token = os.getenv('GITHUB_TOKEN')
-    if not token:
-        raise ValueError("GITHUB_TOKEN environment variable not set")
-    return token
-
-
-def download_labels(repo: github.Repository) -> None:
-    existing_labels: Dict[str, GitHubLabel] = {label.name: label for label in repo.get_labels()}
-    labels_config: Dict[str, GitHubLabelBotLabel] = {}
-    for label_name, label_info in existing_labels.items():
-        print(f"[DEBUG] Sync label {label_name}!")
-        labels_config[label_name] = GitHubLabelBotLabel(
-            color=label_info.color,
-            description=label_info.description,
+        config = GitHubLabelManagementConfig(
+            repositories=[repo.full_name],
+            labels=labels_config,
         )
-    config = GitHubLabelManagementConfig(
-        repositories=[repo.full_name],
-        labels=labels_config,
-    )
-    print("[DEBUG] All labels has been sync!")
-    print(f"[DEBUG] Config: {config}")
-    YAML().write(path="./test/_data/github-labels.yaml", mode="w+", config=config.deserialize())
-    print("[DEBUG] Download GitHub label config finish!")
+        print("[DEBUG] All labels has been sync!")
+        print(f"[DEBUG] Config: {config}")
+        YAML().write(path="./test/_data/github-labels.yaml", mode="w+", config=config.deserialize())
+        print("[DEBUG] Download GitHub label config finish!")
 
 
-def download_as_config() -> None:
-    # Load GitHub token from environment variable
-    token = _get_github_token()
+    def download_as_config(self) -> None:
+        # Load GitHub token from environment variable
+        token = self._get_github_token()
 
-    # Initialize GitHub client
-    print("[DEBUG] Connect to GitHub ...")
-    g = Github(token)
+        # Initialize GitHub client
+        print("[DEBUG] Connect to GitHub ...")
+        g = Github(token)
 
-    config = YAML().read(path="./test/_data/github-labels.yaml")
-    print(f"[DEBUG] config: {config}")
-    config_model = GitHubLabelManagementConfig.serialize(config)
+        config = YAML().read(path="./test/_data/github-labels.yaml")
+        print(f"[DEBUG] config: {config}")
+        config_model = GitHubLabelManagementConfig.serialize(config)
 
-    # Process each repository
-    print(f"[DEBUG] config_model.repositories: {config_model.repositories}")
-    for repo in config_model.repositories:
-        try:
-            repo = g.get_repo(repo)
-            print(f"\nProcessing repository: {repo}")
-            download_labels(repo)
-        except github.GithubException as e:
-            print(f"Error processing {repo}: {e}")
+        # Process each repository
+        print(f"[DEBUG] config_model.repositories: {config_model.repositories}")
+        for repo in config_model.repositories:
+            try:
+                repo = g.get_repo(repo)
+                print(f"\nProcessing repository: {repo}")
+                self.download_labels(repo)
+            except github.GithubException as e:
+                print(f"Error processing {repo}: {e}")
--- github-label-management/test/unit_test/manager.py
+++ github-label-management/test/unit_test/manager.py
@@ -3,14 +3,7 @@ import os
 import pytest
 from unittest.mock import MagicMock, Mock
 import yaml
-from github_label_bot.manager import (
-    load_label_config,
-    sync_labels,
-    syncup_as_config,
-    _get_github_token,
-    download_labels,
-    download_as_config,
-)
+from github_label_bot.manager import GitHubLabelBot
 from github_label_bot.model import GitHubLabelManagementConfig, Label as GitHubLabelBotLabel
 from github.Repository import Repository
 
@@ -29,151 +22,156 @@ delete_unused: true
 """
 
 
-# Mock the config file for testing `load_label_config`
-@pytest.fixture()
-def mock_yaml_file(tmp_path):
-    file_path = tmp_path / "config.yaml"
-    with open(file_path, "w") as file:
-        file.write(SAMPLE_YAML)
-    return str(file_path)
+class TestGitHubLabelBot:
+    # Mock the config file for testing `load_label_config`
+    @pytest.fixture(scope="function")
+    def bot(self) -> GitHubLabelBot:
+        return GitHubLabelBot()
+
+    @pytest.fixture()
+    def mock_yaml_file(self, tmp_path):
+        file_path = tmp_path / "config.yaml"
+        with open(file_path, "w") as file:
+            file.write(SAMPLE_YAML)
+        return str(file_path)
 
 
-# Test load_label_config
-def test_load_label_config(mock_yaml_file):
-    config = load_label_config(mock_yaml_file)
-    assert isinstance(config, GitHubLabelManagementConfig)
-    assert "my-org/my-repository" in config.repositories
-    assert "Bug" in config.labels
-    assert config.labels["Bug"].color == "d73a4a"
+    # Test load_label_config
+    def test_load_label_config(self, bot: GitHubLabelBot, mock_yaml_file):
+        config = bot.load_label_config(mock_yaml_file)
+        assert isinstance(config, GitHubLabelManagementConfig)
+        assert "my-org/my-repository" in config.repositories
+        assert "Bug" in config.labels
+        assert config.labels["Bug"].color == "d73a4a"
 
 
-# Mocked GitHub Repository
-@pytest.fixture
-def mock_github_repo(mocker: MockFixture):
-    mock_repo = mocker.MagicMock(spec=Repository)
-    mock_label = mocker.MagicMock()
-    mock_label.name = "Bug"
-    mock_label.color = "d73a4a"
-    mock_label.description = "A bug label"
-    mock_repo.get_labels.return_value = [mock_label]
-    return mock_repo
+    # Mocked GitHub Repository
+    @pytest.fixture
+    def mock_github_repo(self, mocker: MockFixture):
+        mock_repo = mocker.MagicMock(spec=Repository)
+        mock_label = mocker.MagicMock()
+        mock_label.name = "Bug"
+        mock_label.color = "d73a4a"
+        mock_label.description = "A bug label"
+        mock_repo.get_labels.return_value = [mock_label]
+        return mock_repo
 
 
-# Test sync_labels
-def test_sync_labels_update(mocker: MockFixture, mock_github_repo):
-    mock_repo = mock_github_repo
+    # Test sync_labels
+    def test_sync_labels_update(self, bot: GitHubLabelBot, mocker: MockFixture, mock_github_repo):
+        mock_repo = mock_github_repo
 
-    # Mock configuration for testing
-    label_config = GitHubLabelManagementConfig(
-        repositories=["mock/repository"],
-        labels={
-            "Bug": GitHubLabelBotLabel(color="ffffff", description="New description"),
-        },
-        delete_unused=False,
-    )
+        # Mock configuration for testing
+        label_config = GitHubLabelManagementConfig(
+            repositories=["mock/repository"],
+            labels={
+                "Bug": GitHubLabelBotLabel(color="ffffff", description="New description"),
+            },
+            delete_unused=False,
+        )
 
-    # Call the function
-    sync_labels(mock_repo, label_config)
+        # Call the function
+        bot.sync_labels(mock_repo, label_config)
 
-    # Assert that label.edit was called with the updated properties
-    mock_repo.get_labels.assert_called_once()
-    mock_repo.get_labels.return_value[0].edit.assert_called_with(
-        name="Bug", color="ffffff", description="New description"
-    )
-
-
-# Test sync_labels with creating new labels
-def test_sync_labels_create(mocker: MockFixture, mock_github_repo):
-    mock_repo = mock_github_repo
-
-    # Empty existing labels
-    mock_repo.get_labels.return_value = []
-
-    # Mock configuration for testing
-    label_config = GitHubLabelManagementConfig(
-        repositories=["mock/repository"],
-        labels={
-            "NewLabel": GitHubLabelBotLabel(color="000000", description="A new label"),
-        },
-        delete_unused=False,
-    )
-
-    # Call the function
-    sync_labels(mock_repo, label_config)
-
-    # Assert that create_label was called for the new label
-    mock_repo.create_label.assert_called_once_with(
-        name="NewLabel", color="000000", description="A new label"
-    )
+        # Assert that label.edit was called with the updated properties
+        mock_repo.get_labels.assert_called_once()
+        mock_repo.get_labels.return_value[0].edit.assert_called_with(
+            name="Bug", color="ffffff", description="New description"
+        )
 
 
-# Test _get_github_token with a valid token
-def test__get_github_token(monkeypatch):
-    monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
-    token = _get_github_token()
-    assert token == "mock_token"
+    # Test sync_labels with creating new labels
+    def test_sync_labels_create(self, bot: GitHubLabelBot, mocker: MockFixture, mock_github_repo):
+        mock_repo = mock_github_repo
+
+        # Empty existing labels
+        mock_repo.get_labels.return_value = []
+
+        # Mock configuration for testing
+        label_config = GitHubLabelManagementConfig(
+            repositories=["mock/repository"],
+            labels={
+                "NewLabel": GitHubLabelBotLabel(color="000000", description="A new label"),
+            },
+            delete_unused=False,
+        )
+
+        # Call the function
+        bot.sync_labels(mock_repo, label_config)
+
+        # Assert that create_label was called for the new label
+        mock_repo.create_label.assert_called_once_with(
+            name="NewLabel", color="000000", description="A new label"
+        )
 
 
-# Test _get_github_token when no token is set
-def test__get_github_token_no_env_var(monkeypatch):
-    monkeypatch.delenv("GITHUB_TOKEN", raising=False)
-    with pytest.raises(ValueError, match="GITHUB_TOKEN environment variable not set"):
-        _get_github_token()
+    # Test _get_github_token with a valid token
+    def test__get_github_token(self, bot: GitHubLabelBot, monkeypatch):
+        monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
+        token = bot._get_github_token()
+        assert token == "mock_token"
 
 
-# Test download_labels
-def test_download_labels(mocker: MockFixture, mock_github_repo):
-    mock_repo = mock_github_repo
-    mock_yaml = mocker.patch("github_label_bot.manager.YAML")
-
-    # Call the function
-    download_labels(mock_repo)
-
-    # Assert that YAML().write() was called with appropriate arguments
-    mock_yaml().write.assert_called_once()
-    written_config = mock_yaml().write.call_args[1]["config"]
-    print(f"[DEBUG] written_config: {written_config}")
-    assert "Bug" in written_config['labels'].keys()
-    assert written_config['labels']["Bug"]['color'] == "d73a4a"
+    # Test _get_github_token when no token is set
+    def test__get_github_token_no_env_var(self, bot: GitHubLabelBot, monkeypatch):
+        monkeypatch.delenv("GITHUB_TOKEN", raising=False)
+        with pytest.raises(ValueError, match="GITHUB_TOKEN environment variable not set"):
+            bot._get_github_token()
 
 
-# Test download_as_config
-def test_download_as_config(mocker: MockFixture, monkeypatch):
-    mock_yaml = mocker.patch("github_label_bot.manager.YAML")
-    mock_yaml().read.return_value = {"repositories": ["my-org/my-repository"]}
+    # Test download_labels
+    def test_download_labels(self, bot: GitHubLabelBot, mocker: MockFixture, mock_github_repo):
+        mock_repo = mock_github_repo
+        mock_yaml = mocker.patch("github_label_bot.manager.YAML")
 
-    # Mock the token retrieval
-    monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
+        # Call the function
+        bot.download_labels(mock_repo)
 
-    # Mock the GitHub client and repository
-    mock_github = mocker.patch("github_label_bot.manager.Github")
-    mock_repo = mock_github().get_repo.return_value
-    mock_download_labels = mocker.patch("github_label_bot.manager.download_labels")
-
-    # Call the function
-    download_as_config()
-
-    # Assert that download_labels was called with the correct repository
-    mock_yaml().read.assert_called_once()
-    mock_download_labels.assert_called_once_with(mock_repo)
+        # Assert that YAML().write() was called with appropriate arguments
+        mock_yaml().write.assert_called_once()
+        written_config = mock_yaml().write.call_args[1]["config"]
+        print(f"[DEBUG] written_config: {written_config}")
+        assert "Bug" in written_config['labels'].keys()
+        assert written_config['labels']["Bug"]['color'] == "d73a4a"
 
 
-# Test syncup_as_config
-def test_syncup_as_config(mocker: MockFixture, monkeypatch, mock_github_repo, mock_yaml_file):
-    # Mock the token retrieval
-    monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
+    # Test download_as_config
+    def test_download_as_config(self, bot: GitHubLabelBot, mocker: MockFixture, monkeypatch):
+        mock_yaml = mocker.patch("github_label_bot.manager.YAML")
+        mock_yaml().read.return_value = {"repositories": ["my-org/my-repository"]}
 
-    # Mock the GitHub client
-    mock_github = mocker.patch("github_label_bot.manager.Github")
-    mock_repo = mock_github().get_repo.return_value
-    mock_repo.get_labels.return_value = []
+        # Mock the token retrieval
+        monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
 
-    # Mock configuration loading
-    mocker.patch("github_label_bot.manager.load_label_config", return_value=load_label_config(mock_yaml_file))
+        # Mock the GitHub client and repository
+        mock_github = mocker.patch("github_label_bot.manager.Github")
+        mock_repo = mock_github().get_repo.return_value
+        mock_download_labels = mocker.patch("github_label_bot.manager.GitHubLabelBot.download_labels")
 
-    # Call the function
-    syncup_as_config()
+        # Call the function
+        bot.download_as_config()
 
-    # Assert that repository was fetched and labels were synced
-    mock_github().get_repo.assert_called()
-    mock_repo.create_label.assert_called()
+        # Assert that download_labels was called with the correct repository
+        mock_yaml().read.assert_called_once()
+        mock_download_labels.assert_called_once_with(mock_repo)
+
+
+    # Test syncup_as_config
+    def test_syncup_as_config(self, bot: GitHubLabelBot, mocker: MockFixture, monkeypatch, mock_github_repo, mock_yaml_file):
+        # Mock the token retrieval
+        monkeypatch.setenv("GITHUB_TOKEN", "mock_token")
+
+        # Mock the GitHub client
+        mock_github = mocker.patch("github_label_bot.manager.Github")
+        mock_repo = mock_github().get_repo.return_value
+        mock_repo.get_labels.return_value = []
+
+        # Mock configuration loading
+        mocker.patch("github_label_bot.manager.GitHubLabelBot.load_label_config", return_value=bot.load_label_config(mock_yaml_file))
+
+        # Call the function
+        bot.syncup_as_config()
+
+        # Assert that repository was fetched and labels were synced
+        mock_github().get_repo.assert_called()
+        mock_repo.create_label.assert_called()
