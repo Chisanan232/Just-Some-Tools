49f155a68e0db2abdd452c33e746b173dc4109da
Chisanan232 <chi10211201@cycu.org.tw>
2025-04-02 15:21:53 +0800

    ➕ Add new data model about the input values from GitHub Action.

---
+++ github-label-management/test/unit_test/github_action.py
@@ -0,0 +1,29 @@
+import os
+from typing import List, Dict
+from unittest.mock import patch
+
+import pytest
+
+from github_label_bot.enums import Operation
+from github_label_bot.github_action import GitHubAction
+
+
+class TestGitHubAction:
+
+    @pytest.mark.parametrize(
+        ("mock_env", "expect"),
+        [
+            ({"OPERATIONS": "sync"}, [Operation.Sync]),
+            ({"OPERATIONS": "sync,sync_upstream"}, [Operation.Sync, Operation.Sync_UpStream]),
+            ({"OPERATIONS": "sync_download,sync,sync_upstream"}, [Operation.Sync_Download, Operation.Sync, Operation.Sync_UpStream]),
+            ({"OPERATIONS": "sync_upstream"}, [Operation.Sync_UpStream]),
+        ],
+    )
+    def test_from_env_valid_env_vars(self, mock_env: Dict[str, str], expect: List[Operation]):
+        with patch.dict(os.environ, mock_env, clear=True):
+            action = GitHubAction.from_env()
+            assert action.operation == expect
+
+    def test_from_env_missing_env_vars(self):
+        with pytest.raises(ValueError, match=r"OPERATIONS.{0,64}not set.{0,64}empty."):
+            GitHubAction.from_env()
