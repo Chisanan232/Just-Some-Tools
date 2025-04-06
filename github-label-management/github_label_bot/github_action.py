da5bc45658c68384cb6884e1aa12faaff57c4343
Chisanan232 <chi10211201@cycu.org.tw>
2025-04-06 15:26:48 +0800

    ➕ Add new property about configuration path.

--- github-label-management/github_label_bot/github_action.py
+++ github-label-management/github_label_bot/github_action.py
@@ -8,11 +8,13 @@ from github_label_bot.enums import Operation
 
 @dataclass
 class GitHubAction:
+    config_path: str
     operation: List[Operation]
 
     @staticmethod
     def from_env() -> "GitHubAction":
+        config_path = os.getenv("CONFIG_PATH")
         operations_env = os.getenv("OPERATIONS")
         if not operations_env:
             raise ValueError("Environment variable 'OPERATIONS' is not set or empty.")
-        return GitHubAction(operation=[Operation.to_enum(o) for o in operations_env.split(",")])
+        return GitHubAction(config_path=config_path, operation=[Operation.to_enum(o) for o in operations_env.split(",")])
--- github-label-management/test/unit_test/github_action.py
+++ github-label-management/test/unit_test/github_action.py
@@ -11,17 +11,18 @@ from github_label_bot.github_action import GitHubAction
 class TestGitHubAction:
 
     @pytest.mark.parametrize(
-        ("mock_env", "expect"),
+        ("mock_env", "expect_operations"),
         [
-            ({"OPERATIONS": "sync_download"}, [Operation.Sync_Download]),
-            ({"OPERATIONS": "sync_download,sync_upstream"}, [Operation.Sync_Download, Operation.Sync_UpStream]),
-            ({"OPERATIONS": "sync_upstream"}, [Operation.Sync_UpStream]),
+            ({"CONFIG_PATH": "./test-github-labels.yaml", "OPERATIONS": "sync_download"}, [Operation.Sync_Download]),
+            ({"CONFIG_PATH": "./test-github-labels.yaml", "OPERATIONS": "sync_download,sync_upstream"}, [Operation.Sync_Download, Operation.Sync_UpStream]),
+            ({"CONFIG_PATH": "./test-github-labels.yaml", "OPERATIONS": "sync_upstream"}, [Operation.Sync_UpStream]),
         ],
     )
-    def test_from_env_valid_env_vars(self, mock_env: Dict[str, str], expect: List[Operation]):
+    def test_from_env_valid_env_vars(self, mock_env: Dict[str, str], expect_operations: List[Operation]):
         with patch.dict(os.environ, mock_env, clear=True):
             action = GitHubAction.from_env()
-            assert action.operation == expect
+            assert action.config_path == "./test-github-labels.yaml"
+            assert action.operation == expect_operations
 
     def test_from_env_missing_env_vars(self):
         with pytest.raises(ValueError, match=r"OPERATIONS.{0,64}not set.{0,64}empty."):
